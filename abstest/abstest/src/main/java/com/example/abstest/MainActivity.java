fb761fc7e9b48b4a47eda38d97c78641de2eb5d8
kimukou_26 <kimukou.buzz@gmail.com>
2013-06-06 22:28:23 +0900

    no message

--- abstest/abstest/src/main/java/com/example/abstest/MainActivity.java
+++ abstest/abstest/src/main/java/com/example/abstest/MainActivity.java
@@ -1,20 +1,52 @@
 package com.example.abstest;
 
 import android.os.Bundle;
+
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
+import com.actionbarsherlock.view.MenuInflater;
+import android.support.v4.app.FragmentTransaction;
+
+
 import com.example.lib_yutori.hogeDriven;
 
-public class MainActivity extends SherlockActivity {
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.OptionsMenu;
+
+@EActivity(R.layout.activity_main)
+//@OptionsMenu(R.menu.main)
+public class MainActivity extends SherlockActivity implements ActionBar.TabListener{
+
+    //ActionBarSherlock 
+    //see http://blog.water-cell.jp/2012/08/abs-replace/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.Theme_Sherlock);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+	
+	//Tabの利用
+	final ActionBar mActionBar = getSupportActionBar();
+	mActionBar.addTab(mActionBar.newTab().setText("tab1").setTabListener(this));
+	mActionBar.addTab(mActionBar.newTab().setText("tab2").setTabListener(this));
+	mActionBar.addTab(mActionBar.newTab().setText("tab3").setTabListener(this));
+	mActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+	
+	
         hogeDriven a = new hogeDriven();
         a.print();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getSupportMenuInflater().inflate(R.menu.main, menu);
+	return true;
+    }
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
@@ -22,5 +54,20 @@ public class MainActivity extends SherlockActivity {
 //        getMenuInflater().inflate(R.menu.main, menu);
 //        return true;
 //    }
-    
+ 
+
+
+    @Override
+    public void onTabSelected(Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+    }
+
+    @Override
+    public void onTabReselected(Tab tab,FragmentTransaction ft){
+
+    }
+   
 }
